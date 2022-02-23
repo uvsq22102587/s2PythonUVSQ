bcd4ac32bf24add00a080aa99e6e899c2c0cb7d5
Valentin <valentin.jacquin@ens.uvsq.fr>
2022-02-23 19:55:17 +0100

    Fin feuille 2

--- Exercices/Feuille 2/Exercice 1.py
+++ Exercices/Feuille 2/Exercice 1.py
@@ -1,4 +1,3 @@
-from multiprocessing import Condition
 import tkinter as tk
 import random as rdm
 
@@ -24,13 +23,22 @@ def mouvement(balle):
     yPlus = balle[1]
     xPlus = balle[2]
     cCanevas.move(cercle, xPlus, yPlus)
+    coords = cCanevas.coords(balle[0])
+    if coords[0] <= 0:
+        rebond1(balle, 0)
+    if coords[2] >= 600:
+        rebond1(balle, 2)
+    if coords[1] <= 0:
+        rebond1(balle, 1)
+    if coords[3] >= 400:
+        rebond1(balle, 3)
     if cond is True:
         bStop.grid_remove()
         bStart.grid(column=0, row=1)
         cCanevas.after_cancel(balle)
         cond = False
         return
-    cCanevas.after(20, lambda: mouvement(balle))
+    cCanevas.after(10, lambda: mouvement(balle))
 
 
 def stop():
@@ -38,10 +46,30 @@ def stop():
     cond = True
 
 
-def rebond1(balle):
-    balle[1] = - balle[1]
-    balle[2] = - balle[2]
+def rebond1(balle, bords):
+    if bords == 1 or bords == 3:
+        balle[1] = - balle[1]
+    if bords == 0 or bords == 2:
+        balle[2] = - balle[2]
     return balle
+
+
+def rebond2(balle, bords):
+    coords = cCanevas.coords(balle[0])
+    if bords == 0:
+        cCanevas.delete(balle[0])
+        balle[0] = cCanevas.create_oval((599, coords[1]), (559, coords[3]), fill="Blue")
+    if bords == 1:
+        cCanevas.delete(balle[0])
+        balle[0] = cCanevas.create_oval((coords[0], 399), (coords[2], 359), fill="Blue")
+    if bords == 2:
+        cCanevas.delete(balle[0])
+        balle[0] = cCanevas.create_oval((1, coords[1]), (41, coords[3]), fill="Blue")
+    if bords == 3:
+        cCanevas.delete(balle[0])
+        balle[0] = cCanevas.create_oval((coords[0], 1), (coords[2], 41), fill="Blue")
+    return
+
 
 bStop = tk.Button(root, background="blue", text="Stop!", command=stop)
 balle = creer_balle()
