41515e49e572c0aaf5c401dbe8d134bf7ca17189
Valentin <valentin.jacquin@ens.uvsq.fr>
2022-02-28 18:31:47 +0100

    Dm IN202 Avancement + upload

---
+++ Exercices/DM IN202/1/projet.py
@@ -0,0 +1,22 @@
+import PIL as pil
+from PIL import Image
+from PIL import ImageTk 
+
+
+def saving(matPix, filename):
+    #sauvegarde l'image contenue dans matpix dans le fichier filename
+	#utiliser une extension png pour que la fonction fonctionne sans perte d'information
+    toSave=pil.Image.new(mode = "1", size = (nbrCol(matPix),nbrLig(matPix)))
+    for i in range(nbrLig(matPix)):
+        for j in range(nbrCol(matPix)):
+            toSave.putpixel((j,i),matPix[i][j])
+    toSave.save(filename)
+
+def loading(filename):#charge le fichier image filename et renvoie une matrice de 0 et de 1 qui représente 
+					  #l'image en noir et blanc
+    toLoad = pil.Image.open(filename)
+    mat = [[0]*toLoad.size[0] for k in range(toLoad.size[1])]
+    for i in range(toLoad.size[1]):
+        for j in range(toLoad.size[0]):
+            mat[i][j]= 0 if toLoad.getpixel((j,i)) == 0 else 1
+    return mat
