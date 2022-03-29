64edc6253ef8df1d45befdf8948614df00849b35
Valentin <valentin.jacquin@ens.uvsq.fr>
2022-03-29 20:18:53 +0200

    Avancement QR Code

--- Kholles/1/balle.py
+++ Kholles/1/balle.py
@@ -13,6 +13,8 @@ HAUTEUR = 400
 def creer_balle():
     """Dessine un disque bleu et retourne son identifiant
      et les valeurs de déplacements dans une liste"""
+    global compteur
+    compteur = 0
     x, y = LARGEUR // 2, HAUTEUR // 2
     dx, dy = 3, 5
     rayon = 20
@@ -26,17 +28,21 @@ def mouvement():
     """Déplace la balle et ré-appelle la fonction avec un compte-à-rebours"""
     rebond()
     canvas.move(balle[0], balle[1], balle[2])
-    canvas.after(20, mouvement)
+    if compteur <= 5:
+        canvas.after(20, mouvement)
+    
 
 
 def rebond():
     """Fait rebondir la balle sur les bords du canevas"""
-    global balle
+    global balle, compteur
     x0, y0, x1, y1 = canvas.coords(balle[0])
     if x0 <= 0 or x1 >= 600:
         balle[1] = -balle[1]
+        compteur += 1
     if y0 <= 0 or y1 >= 400:
         balle[2] = -balle[2]
+        compteur += 1
 
 
 ######################
